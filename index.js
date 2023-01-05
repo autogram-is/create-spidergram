a9c848ea60df51131163a97fd7e55c3c5644419e
Jeff Eaton <jeff@eaton.fyi>
2023-01-04 21:59:34 -0600

    Creating the actual creator

--- index.js
+++ index.js
@@ -1,12 +1,76 @@
+import { fileURLToPath } from 'node:url';
+import * as path from 'node:path';
+import inquirer from 'inquirer';
+import { readJsonSync, writeJsonSync, copySync, removeSync } from 'fs-extra/esm';
+import { readdir } from 'node:fs';
 
-import { join } from 'node:path';
+var template;
+var skipPrompts;
+var projectName;
 
-const args = process.argv.slice(2);
-const template = args[0];
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const workingDirectory = process.cwd();
-const templateDirectory = join(__dirname, 'projects');
+for (const arg of process.argv.slice(2)) {
+  if (arg.startsWith('--template')) template = arg.split('=')[1];
+  if (arg.startsWith('-y')) skipPrompts = true;
+  else projectName = arg;
+}
 
-console.log(template);
-console.log(workingDirectory);
-console.log(templateDirectory);
+template ??= 'boilerplate-ts';
+projectName ??= 'my-spider';
+
+try {
+  if (skipPrompts) {
+    clone(projectName, template)
+  } else {
+    const answers = await setup();
+    clone(answers.name, answers.template);
+  }
+} catch(err) {
+  console.log(`ERROR: ${err.message}`);
+}
+
+async function setup() {
+  const questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Project name',
+      default: projectName
+    },
+    {
+      type: 'list',
+      name: 'template',
+      message: 'Project template',
+      choices: [
+        { name: 'Simple crawler' , value: 'boilerplate' },
+        { name: 'Simple crawler (Typescript)' , value: 'boilerplate-ts' },
+        { name: 'Queries and charts example (Typescript)' , value: 'example-plugins-ts' },
+        { name: 'Site audit example (Typescript)' , value: 'example-audit-ts' }
+      ],
+      default: template
+    }
+  ];
+  
+  return inquirer.prompt(questions);
+}
+
+function clone(name, template) {
+  const ignore = ['./DS_Store', 'node_modules'];
+  const templateDir = path.join(__dirname, 'projects', template);
+  const targetDir = process.cwd();
+
+  readdir(targetDir, (err, files) => {
+    if (err || files.length > 0) {
+      console.log(files);
+      throw new Error('Directory is not empty!');
+    }
+    copySync(templateDir, targetDir, { overwrite: true });
+    const pkg = readJsonSync(path.join(targetDir, 'package.json'));
+    writeJsonSync(path.join(targetDir, 'package.json'), pkg, { spaces: 2 });
+    for (const file of ignore) {
+      removeSync(path.join(targetDir, file));
+    }
+  });
+}
